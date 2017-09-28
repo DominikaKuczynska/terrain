40ab4784677042671d43ec5dfc55bca40815ea6d
Dominika Kuczynska <dominika.kuczynska@gmail.com>
2017-09-28 19:17:33 +0200

    git init

---
+++ Assets/tala.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tala : MonoBehaviour {
+
+	string zmienna = "Słowo";
+	string zmienna2 = "Wyraz";
+	int liczba = 98 ;
+	int liczba2 = 77;
+	// Use this for initialization
+	void Start () {
+		float zmienna1 = 5.0f/2.0f;
+
+
+		print(zmienna1); //wyświetli ?
+
+
+
+
+		zmienna1 = 5.0f/2;
+
+
+		print(zmienna1); //wyświetli ?
+
+
+
+
+		zmienna1 = 5/2.0f;
+
+
+		print(zmienna1); //wyświetli ?
+
+
+
+
+		zmienna1 = 5/2;
+
+
+		print(zmienna1); //wyświetli ?
+		print (liczba);
+		print (zmienna);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
